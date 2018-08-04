86492232e52b742a06451de9bd5654caf9867132
Bruno <brunofunk@gmail.com>
2018-08-04 19:34:55 -0300

    Estado e cidade

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.domain.Estado;
 import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.CidadeRepository;
+import com.nelioalves.cursomc.repositories.EstadoRepository;
 import com.nelioalves.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -41,7 +51,20 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado minas = new Estado(null, "Minas Gerais");
+		Estado saoPaulo = new Estado(null, "São Paulo");
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Cidade uberlandia = new Cidade(null, "Uberlandia", minas);
+		Cidade sp = new Cidade(null, "São Paulo", saoPaulo);
+		Cidade campinas = new Cidade(null, "Campinas", saoPaulo);
+		
+		minas.getCidades().add(uberlandia);
+		saoPaulo.getCidades().addAll(Arrays.asList(sp, campinas));
+		
+		estadoRepository.saveAll(Arrays.asList(minas, saoPaulo));
+		cidadeRepository.saveAll(Arrays.asList(uberlandia, sp, campinas));
 	}
 }
