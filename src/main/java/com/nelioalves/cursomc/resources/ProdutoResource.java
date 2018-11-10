2209514b5d966365722cdd01a44b4ffae81e89fa
Bruno <brunofunk@gmail.com>
2018-11-10 18:28:15 -0200

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
@@ -0,0 +1,48 @@
+package com.nelioalves.cursomc.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Produto;
+import com.nelioalves.cursomc.dto.ProdutoDTO;
+import com.nelioalves.cursomc.resources.utils.URL;
+import com.nelioalves.cursomc.services.ProdutoService;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoResource {
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Produto> find(@PathVariable Integer id) {
+		Produto produto = produtoService.find(id);
+		return ResponseEntity.ok().body(produto);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value="nome", defaultValue="") String nome,
+			@RequestParam(value="categorias", defaultValue="") String categorias,
+			@RequestParam(value="page", defaultValue="0")Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome")String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC")String direction) {
+		String nomeDecodificado = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Produto> produtos = produtoService.search(nomeDecodificado, ids, page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> listaDTO = produtos.map(produto -> new ProdutoDTO(produto));
+		return ResponseEntity.ok().body(listaDTO);
+	}
+	
+
+}
