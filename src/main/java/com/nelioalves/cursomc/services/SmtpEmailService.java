047f1a5835a01c834a6c5837e440ef8c498e32fc
Bruno <brunofunk@gmail.com>
2018-12-10 00:08:39 -0200

    Email html

--- src/main/java/com/nelioalves/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 	
@@ -12,11 +15,22 @@ public class SmtpEmailService extends AbstractEmailService {
 	
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		log.info("Enviando email...");
 		mailSender.send(msg);
+		log.info("email enviado");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		log.info("Enviando email HTML...");
+		javaMailSender.send(msg);
 		log.info("email enviado");
 		
 	}
