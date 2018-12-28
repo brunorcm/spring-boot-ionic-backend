0b6fe80fad2f6b7aa01b61f509e1d4c49841f382
Bruno <brunofunk@gmail.com>
2018-12-27 23:31:02 -0200

    enviando imagem via endpoint

--- src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Cliente;
@@ -78,6 +79,12 @@ public class ClienteResource {
 		Page<Cliente> clientes = clienteService.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listaDTO = clientes.map(cliente -> new ClienteDTO(cliente));
 		return ResponseEntity.ok().body(listaDTO);
+	}
+	
+	@RequestMapping(value = "/picture", method = RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		URI uri = clienteService.uploadProfilePicture(file);		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.nelioalves.cursomc.domain.Cidade;
 import com.nelioalves.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -101,6 +106,10 @@ public class ClienteService {
 			cli.getTelefones().add(dto.getTelefone3());
 		}
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
